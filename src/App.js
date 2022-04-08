97ea4342ff374836a450b7a703eccf44837901f2
Shubham <deshpande460@gmail.com>
2022-04-08 13:34:39 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import React, { useState } from "react";
 import styledComponents from "styled-components";
 import MovieComponent from "./components/MovieComponent";
 import MovieInfoComponent from "./components/MovieInfoComponent";
-export const API_KEY = 'c77084dd'
-
-
-
+export const API_KEY = "c77084dd";
 
 const Container = styledComponents.div`
 display: flex;
@@ -23,18 +20,14 @@ padding:10px;
 font-size:25px;
 font-weight:bold;
 box-shadow:0 3px 6px 0 #555;
-`
+`;
 const AppName = styledComponents.div`
 display:flex;
 flex-direction:row;
 align-items:center;
-`
-const MovieImg= styledComponents.img`
-width:48px;
-height:48px;
-margin:15px;
-`
-const SearchBox=styledComponents.div`
+`;
+
+const SearchBox = styledComponents.div`
 display:flex;
 flex-direction:row;
 padding: 10px 10px;
@@ -45,12 +38,9 @@ width:50%;
 background-color:white;
 align-items:center;
 
-`  
-const SearchIcon = styledComponents.img`
-width:32px;
-height:32px;
-`
-const SearchInput=styledComponents.input`
+`;
+
+const SearchInput = styledComponents.input`
 color:black;
 font-size:16px;
 font-weight:bold;
@@ -58,7 +48,7 @@ border:none;
 outline:none;
 margin-left:15px;
 `;
-const MovieListContainer=styledComponents.div`
+const MovieListContainer = styledComponents.div`
 display:flex;
 flex-direction:row;
 gap:24px;
@@ -68,51 +58,55 @@ justify-content:space-evenly;
 `;
 
 function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [timeoutId, updateTimeoutId] = useState();
+  const [movieList, updatemovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
 
-const[searchQuery, updateSearchQuery]= useState("");
-const[timeoutId, updateTimeoutId]= useState();
-const[movieList, updatemovieList]= useState([]);
-const[selectedMovie, onMovieSelect]= useState();
+  const fetchData = async (searchString) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+    );
 
-const fetchData =async (searchString)=>{
- const response=await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`);
- 
- updatemovieList(response.data.Search);
-}
+    updatemovieList(response.data.Search);
+  };
 
-const onTextChange=(event)=>{
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId);
+    updateSearchQuery(event.target.value);
+    const timeout = setTimeout(() => fetchData(event.target.value), 500);
+    updateTimeoutId(timeout);
+  };
 
-  clearTimeout(timeoutId);
-  updateSearchQuery(event.target.value);
-  const timeout = setTimeout(()=>fetchData(event.target.value),500);
-  updateTimeoutId(timeout);
-};
-
-  return <Container>
-    <Header>
-      <AppName>
-        <MovieImg src="/movie-icon.svg"/>
-        React Movie App</AppName>
+  return (
+    <Container>
+      <Header>
+        <AppName>
+         
+          React Movie App
+        </AppName>
         <SearchBox>
-<SearchIcon src="/search-icon.svg"/>
-<SearchInput placeholder="Search Movie" value={searchQuery}
- onChange={onTextChange}/>
+         
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
         </SearchBox>
       </Header>
       <MovieListContainer>
-
-        {movieList?.length?
-         movieList.map((movie,index)=>(
-        <MovieComponent key={index} movie={movie} onMovieSelect={onMovieSelect}/>
-        ))
-        :"No Movie Search"}
-        
-       
+        {movieList?.length
+          ? movieList.map((movie, index) => (
+              <MovieComponent
+                key={index}
+                movie={movie}
+                onMovieSelect={onMovieSelect}
+              />
+            ))
+          : "No Movie Search"}
       </MovieListContainer>
     </Container>
-   
+  );
 }
-
-
 
 export default App;
--- src/components/MovieInfoComponent.js
+++ src/components/MovieInfoComponent.js
@@ -1,12 +1,12 @@
 
 
-// const MovieInfoComponent=(props)=>{
-//     const {Title, Year, imdbID, Type, Poster} = props.movie;
+const MovieInfoComponent=(props)=>{
+    const {Title, Year, imdbID, Type, Poster} = props.movie;
     
-//     return(
-//     <>
+    return(
+    <>
 
-//     </>
-//     );
-//     };
-//     export default MovieInfoComponent;
+    </>
+    );
+    };
+    export default MovieInfoComponent;
